48b058eb7eab18e69038ed6245078d091d2feb67
DESKTOP-2QGAPE9\Dang Nguyen <The_doomgard@yahoo.com>
2018-12-17 22:04:47 -0800

    Adding ship layout of spaceship level

--- Hive Mind/Assets/DangNguyen/DangScripts/DN_SpaceShipControl.cs
+++ Hive Mind/Assets/DangNguyen/DangScripts/DN_SpaceShipControl.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DN_SpaceShipControl : MonoBehaviour {
-
+    public bool UpPad;
+    public bool DownPad;
+    public bool ShootLeft;
+    public bool ShootRight;
+    public bool Shield;
+    public bool FixShip;
+    public bool Grab;
 	// Use this for initialization
 	void Start () {
 		
