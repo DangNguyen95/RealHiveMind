6b8c37f71ef07220e2cdda75bcc994b00407f3d7
Nmonta <45674030+Nmonta@users.noreply.github.com>
2019-01-15 17:54:33 -0700

    Color Randomizer
    Random color work

---
+++ Hive Mind/Assets/NoelMontalvo/MontalvoScripts/NM_CRandomizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NM_CRandomizer : MonoBehaviour {
+
+
+    public Color[] Rcolors;
+    public Color[] Scolors;
+    public GameObject cubeobject;
+    public int startnum;
+	// Use this for initialization
+	void Start () {
+        startnum = 0;
+        StartCoroutine(pauseamoment());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RandomMyColors()
+    {
+       
+       
+    }
+
+    IEnumerator pauseamoment()
+    {
+        for (int i =startnum; i < Scolors.Length; i++)
+        {
+            startnum = i;
+            Scolors[i] = Rcolors[Random.Range(0, 4)];
+            cubeobject.GetComponent<Renderer>().material.color = Scolors[i];
+            yield return new WaitForSeconds(1);
+            cubeobject.GetComponent<Renderer>().material.color = Color.white;
+            yield return new WaitForSeconds(1);
+
+          if(i >= Scolors.Length)
+            {
+                StopCoroutine(pauseamoment());
+
+            }
+        }
+
+        
+       
+       
+
+
+    }
+}
