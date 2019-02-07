15f17270b65ab04c72493df96f3b9d8fd15e1c6c
August590 <45698699+August590@users.noreply.github.com>
2019-02-07 04:12:00 -0600

    Setup pattern stuff
    Patterns are now cycle through and rounds can be added as well as a reset for the fire pits and pfi

--- Hive Mind/Assets/AugustLay/Scripts/AL_FireManager.cs
+++ Hive Mind/Assets/AugustLay/Scripts/AL_FireManager.cs
@@ -6,6 +6,9 @@ public class AL_FireManager : MonoBehaviour {
     [SerializeField]
     List<GameObject> lavaTraps = new List<GameObject>();
 
+
+    DN_GameManager managerGame;
+
     [System.Serializable]
     public class myPatternNumbers
     {
@@ -13,46 +16,72 @@ public class AL_FireManager : MonoBehaviour {
         public int[] myNumbers;
     }
     [SerializeField]
-    int blach = 1;
+    int myNumbersMax;
+    [SerializeField]
+    int myCurPat = 0;
+    float myCurFireTime = 2;
+    bool doOnce = false;
     GameObject myPatternObject;
     int[] myNumbsRef;
     public List<myPatternNumbers> myPattern = new List<myPatternNumbers>();
 
     public delegate void FiresToListen();
     public static event FiresToListen FiresToReach;
+    public static event FiresToListen FirePFIReach;
+    public static event FiresToListen FireReset;
 
 
     // Use this for initialization
     void Start () {
+        //managerGame = FindObjectOfType<DN_GameManager>();
         
-        AL_FirePit[] aL_FirePit = FindObjectsOfType<AL_FirePit>();
+        //AL_FirePit[] aL_FirePit = FindObjectsOfType<AL_FirePit>();
 
-        foreach(AL_FirePit element in aL_FirePit)
-        {
-            lavaTraps.Add(element.gameObject);
-            //element.gameObject.SetActive(false);
-        }
-        lavaTraps.Sort(delegate (GameObject a, GameObject b)
-        {
-            return (a.GetComponent<AL_FirePit>().fireNumber).CompareTo(b.GetComponent<AL_FirePit>().fireNumber);
-        });
+        //foreach(AL_FirePit element in aL_FirePit)
+        //{
+        //    lavaTraps.Add(element.gameObject);
+        //    element.gameObject.SetActive(false);
+        //}
+        //lavaTraps.Sort(delegate (GameObject a, GameObject b)
+        //{
+        //    return (a.GetComponent<AL_FirePit>().fireNumber).CompareTo(b.GetComponent<AL_FirePit>().fireNumber);
+        //});
 
-        StartCoroutine(kickOOF());
+        //StartCoroutine(kickOOF(myCurPat, myCurFireTime));
         //firePatternStarter(myPattern[0]);
     }
-    IEnumerator kickOOF()
-    {
-        blach = blach + 1;
-        yield return new WaitForSeconds(1);
-        blach = blach + 1;
-        firePatternStarter(myPattern[0]);
-        //StopCoroutine(kickOOF());
-    }
+    
     // Update is called once per frame
     void Update () {
         //FiresToReach.Invoke();
-    }
+        if (Time.timeScale == 1 && doOnce == false)
+        {
+            doOnce = true;
+            myNumbersMax = myPattern.Count;
+            AL_FirePit[] aL_FirePit = FindObjectsOfType<AL_FirePit>();
 
+            foreach (AL_FirePit element in aL_FirePit)
+            {
+                lavaTraps.Add(element.gameObject);
+                //element.gameObject.SetActive(false);
+            }
+            lavaTraps.Sort(delegate (GameObject a, GameObject b)
+            {
+                return (a.GetComponent<AL_FirePit>().fireNumber).CompareTo(b.GetComponent<AL_FirePit>().fireNumber);
+            });
+
+            StartCoroutine(kickOOF(myCurPat, myCurFireTime));
+        }
+    }
+    IEnumerator kickOOF(int myCurrentPattern, float timeToFire)
+    {
+        //blach = blach + 1;
+        firePatternPFIStarter(myPattern[myCurrentPattern]);
+        yield return new WaitForSeconds(timeToFire);
+        //blach = blach + 1;
+        firePatternStarter(myPattern[myCurrentPattern]);
+        //StopCoroutine(kickOOF());
+    }
 
     void firePatternStarter(myPatternNumbers myNumbs)
     {
@@ -64,6 +93,34 @@ public class AL_FireManager : MonoBehaviour {
             
         }
         FiresToReach.Invoke();
-        //StopCoroutine(kickOOF());
+        StopCoroutine(kickOOF(myCurPat, myCurFireTime));
+        StartCoroutine(resetingFires());
+    }
+    void firePatternPFIStarter(myPatternNumbers myNumbs)
+    {
+        foreach (int element in myNumbs.myNumbers)
+        {
+            
+            GameObject myObject = lavaTraps.Find(x => x.GetComponent<AL_FirePit>().fireNumber == element);
+            myObject.GetComponent<AL_FirePit>().firePFIOn = false;
+
+        }
+        FirePFIReach.Invoke();
+        //StopCoroutine(kickOOF(myCurPat, myCurFireTime));
+    }
+
+    IEnumerator resetingFires()
+    {
+
+        
+            yield return new WaitForSeconds(3);
+            myCurPat++;
+            FireReset.Invoke();
+            yield return new WaitForSeconds(1);
+            StopCoroutine(resetingFires());
+        if (myCurPat < myNumbersMax)
+        {
+            StartCoroutine(kickOOF(myCurPat, myCurFireTime));
+        }
     }
 }
--- Hive Mind/Assets/AugustLay/Scripts/AL_FirePit.cs
+++ Hive Mind/Assets/AugustLay/Scripts/AL_FirePit.cs
@@ -6,9 +6,18 @@ public class AL_FirePit : MonoBehaviour {
     
     public int fireNumber;
     public bool fireOn = true;
+    public bool firePFIOn = true;
+    [SerializeField]
+    GameObject myChildFires;
+    [SerializeField]
+    GameObject myChildPFI;
 	// Use this for initialization
 	void Start () {
         AL_FireManager.FiresToReach += turnMeOnOrOff;
+        AL_FireManager.FirePFIReach += turnMyPFIOnOrOff;
+        AL_FireManager.FireReset += resetFires;
+        myChildFires = transform.GetChild(0).gameObject;
+        myChildPFI = transform.GetChild(1).gameObject;
     }
 	
 	// Update is called once per frame
@@ -21,11 +30,31 @@ public class AL_FirePit : MonoBehaviour {
     {
         if(fireOn == true)
         {
-            gameObject.SetActive(true);
+            //gameObject.SetActive(true);
+            myChildPFI.SetActive(false);
+            myChildFires.SetActive(true);
         }
         if(fireOn == false)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
         }
+    }
+    void turnMyPFIOnOrOff()
+    {
+        if(firePFIOn == true)
+        {
+            myChildPFI.SetActive(true);
+        }
+        else
+        {
+
+        }
+    }
+    void resetFires()
+    {
+        myChildPFI.SetActive(false);
+        myChildFires.SetActive(false);
+        fireOn = true;
+        firePFIOn = true;
     }
 }
