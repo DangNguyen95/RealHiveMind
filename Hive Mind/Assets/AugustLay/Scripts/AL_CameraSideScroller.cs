95dbe952a8a83ea1a9620d67450358c80ea62620
August590 <45698699+August590@users.noreply.github.com>
2019-02-22 00:20:48 -0600

    ScriptForCamera
    FinishedScripting for camera maybe need edits later

--- Hive Mind/Assets/AugustLay/Scripts/AL_CameraSideScroller.cs
+++ Hive Mind/Assets/AugustLay/Scripts/AL_CameraSideScroller.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class AL_CameraSideScroller : MonoBehaviour {
 
-    
+    AL_EndPoint endPoint;
+    [SerializeField]
+    GameObject myEndPoint;
 
-
+    float distanceTraveled;
+    [SerializeField]
+    float timePassing;
 	// Use this for initialization
 	void Start () {
-		
+        endPoint = FindObjectOfType<AL_EndPoint>();
+        myEndPoint = endPoint.gameObject;
+        startToMove();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        timePassing = Time.deltaTime * 0.02f;
+        distanceTraveled = Mathf.Lerp(transform.position.x, myEndPoint.transform.position.x, timePassing);
+        transform.position = new Vector3(distanceTraveled, transform.position.y, transform.position.z);
+        
+    }
+
+    void startToMove()
+    {
+       // Vector3.MoveTowards(transform.position, myEndPoint.transform.position, timePassing);
+    }
+
 }
