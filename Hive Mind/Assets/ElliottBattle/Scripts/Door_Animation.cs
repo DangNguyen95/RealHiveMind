4cdac4186b226ff38cdef361a772abd9cee771e1
ElliottBat <37560906+ElliottBat@users.noreply.github.com>
2019-06-03 17:57:38 -0400

    Setup Ui and Credits

--- Hive Mind/Assets/ElliottBattle/Scripts/Door_Animation.cs
+++ Hive Mind/Assets/ElliottBattle/Scripts/Door_Animation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elliott : MonoBehaviour {
+public class Door_Animation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
