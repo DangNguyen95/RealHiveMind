107a4034418bfc0da3c28f282da3acd0584e4f2f
ElliottBat <37560906+ElliottBat@users.noreply.github.com>
2019-06-19 21:44:49 -0400

    Update Test

--- Hive Mind/Assets/ElliottBattle/Elliott.cs
+++ Hive Mind/Assets/ElliottBattle/Elliott.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door_Animation : MonoBehaviour {
+public class Elliott : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
