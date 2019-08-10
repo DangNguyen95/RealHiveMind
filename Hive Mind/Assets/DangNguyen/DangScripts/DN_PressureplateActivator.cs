f613e37579f9477e1df5d21d254f4a022afb8346
DESKTOP-2QGAPE9\Dang Nguyen <The_doomgard@yahoo.com>
2019-08-09 23:59:19 -0700

    Dang Adjusting Pressure plate

--- Hive Mind/Assets/DangNguyen/DangScripts/DN_PressureplateActivator.cs
+++ Hive Mind/Assets/DangNguyen/DangScripts/DN_PressureplateActivator.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DN_PressureplateActivator : MonoBehaviour {
-    private Animator Pressureplate;
-	// Use this for initialization
-	void Start () {
-        Pressureplate = gameObject.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject SpriteSelf;
+    private Animator SpriteSelfAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpriteSelfAnim = SpriteSelf.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Square")
+        {
+            SpriteSelfAnim.SetBool("Close", true);
+        }
+        if (other.tag == "O")
+        {
+            SpriteSelfAnim.SetBool("Close", true);
+        }
+        if (other.tag == "X")
+        {
+            SpriteSelfAnim.SetBool("Close", true);
+        }
+        if (other.tag == "Triangle")
+        {
+            SpriteSelfAnim.SetBool("Close", true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Square")
+        {
+            SpriteSelfAnim.SetBool("Close", false);
+        }
+        if (other.tag == "O")
+        {
+            SpriteSelfAnim.SetBool("Close", false);
+        }
+        if (other.tag == "X")
+        {
+            SpriteSelfAnim.SetBool("Close", false);
+        }
+        if (other.tag == "Triangle")
+        {
+            SpriteSelfAnim.SetBool("Close", false);
+        }
+    }
 }
