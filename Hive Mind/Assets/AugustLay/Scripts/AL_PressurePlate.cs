d7bd58622d6f15d8c7d3a611c8f9a138cba1a483
DESKTOP-2QGAPE9\Dang Nguyen <The_doomgard@yahoo.com>
2020-02-03 21:10:02 -0800

    adding art to noel simon level

--- Hive Mind/Assets/AugustLay/Scripts/AL_PressurePlate.cs
+++ Hive Mind/Assets/AugustLay/Scripts/AL_PressurePlate.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class AL_PressurePlate : MonoBehaviour {
    
-    public GameObject PlateOn;
+   
     public GameObject PlateOff;
     public GameObject PlateOff2;
- 
+    public GameObject PlateOff3;
+    public GameObject Otherplate1;
+    private AL_PressurePlate PresureplateScript1;
+    private AL_PressurePlate PresureplateScript2;
+    public GameObject Otherplate2;
+    public Transform gateLoc;
+    public Transform GateLoc2;
+    public bool RushLevel;
+    public bool GateSync;
 	// Use this for initialization
 	void Start () {
-      
+      if(RushLevel)
+        {
+            PresureplateScript1 = Otherplate1.GetComponent<AL_PressurePlate>();
+            PresureplateScript2 = Otherplate2.GetComponent<AL_PressurePlate>();
+        }
 	}
 	
 	// Update is called once per frame
@@ -23,27 +35,99 @@ public class AL_PressurePlate : MonoBehaviour {
         
         if(other.tag == "O")
         {
-            PlateOn.SetActive(true);
-            PlateOff.SetActive(false);
-            PlateOff2.SetActive(false);
+            if(RushLevel)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+                GateSync = true;
+
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+            }
+           
+            //PlateOn.SetActive(true);
+            //PlateOff.SetActive(false);
+            //PlateOff2.SetActive(false);
         }
         if (other.tag == "Triangle")
         {
-            PlateOn.SetActive(true);
-            PlateOff.SetActive(false);
-            PlateOff2.SetActive(false);
+            if (RushLevel)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+                GateSync = true;
+
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
         if (other.tag == "X")
         {
-            PlateOn.SetActive(true);
-            PlateOff.SetActive(false);
-            PlateOff2.SetActive(false);
+            if (RushLevel)
+            {
+
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+                GateSync = true;
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
         if (other.tag == "Square")
         {
-            PlateOn.SetActive(true);
-            PlateOff.SetActive(false);
-            PlateOff2.SetActive(false);
+            if (RushLevel)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+                GateSync = true;
+
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = false;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = true;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
     }
 
@@ -51,27 +135,116 @@ public class AL_PressurePlate : MonoBehaviour {
     {
         if (other.tag == "O")
         {
-            PlateOn.SetActive(false);
-            PlateOff.SetActive(true);
-            PlateOff2.SetActive(false);
+            if (RushLevel && PresureplateScript1.GateSync ==false || RushLevel && PresureplateScript2.GateSync == false)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+                PlateOff3.transform.position = GateLoc2.transform.position;
+                GateSync = false;
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+            }
+               
+            //PlateOn.SetActive(false);
+            //PlateOff.SetActive(true);
+            //PlateOff2.SetActive(false);
         }
         if (other.tag == "Triangle")
         {
-            PlateOn.SetActive(false);
-            PlateOff.SetActive(true);
-            PlateOff2.SetActive(false);
+            if (RushLevel && PresureplateScript1.GateSync ==false || RushLevel && PresureplateScript2.GateSync == false)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff3.transform.position = GateLoc2.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+                GateSync = false;
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+                PlateOff3.transform.position = GateLoc2.transform.position;
+            }
         }
         if (other.tag == "X")
         {
-            PlateOn.SetActive(false);
-            PlateOff.SetActive(true);
-            PlateOff2.SetActive(false);
+            if (RushLevel && PresureplateScript1.GateSync==false || RushLevel && PresureplateScript2.GateSync == false)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+                PlateOff3.transform.position = GateLoc2.transform.position;
+                GateSync = false;
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+            }
         }
         if (other.tag == "Square")
         {
-            PlateOn.SetActive(false);
-            PlateOff.SetActive(true);
-            PlateOff2.SetActive(false);
+            if (RushLevel && PresureplateScript1.GateSync == false || RushLevel && PresureplateScript2.GateSync == false)
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+                PlateOff3.transform.position = GateLoc2.transform.position;
+                GateSync = false;
+            }
+            else
+            {
+                PlateOff.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff2.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff2.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff3.GetComponent<Rigidbody>().useGravity = false;
+                PlateOff3.GetComponent<Rigidbody>().isKinematic = true;
+                PlateOff.transform.position = gateLoc.transform.position;
+                PlateOff2.transform.position = gateLoc.transform.position;
+            }
         }
 
     }
